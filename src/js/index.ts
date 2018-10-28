5f7ed1d11d53962e19cb7209ab4c306dcd69dea7
Frederik Skærbæk Jensen <fred15b4@edu.easj.dk>
2018-10-28 11:59:32 +0100

    Interface samt function til at hente kunder fra CustomerRestService er oprettet

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,39 @@
-interface Person {
-    firstName: string;
-    lastName: string;
-}
+import axios, {
+    AxiosResponse, 
+    AxiosError} from "../../node_modules/axios/index";
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-let user: Person = { firstName: "John", lastName: "Doe" };
+    interface ICustomer{
+        id:number;
+        firstName:string;
+        lastName:string;
+        year:number;
+    }
 
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+    let showAllCustomerElement : HTMLDivElement = <HTMLDivElement>document.getElementById("content");
+    let getAllCustomersButton : HTMLButtonElement = <HTMLButtonElement>document.getElementById("getAllCustomerBtn");
+    getAllCustomersButton.addEventListener("click", showAllCustomers);
+    let uri : string = "https://restcustomerservice20181024123050.azurewebsites.net/api/customer";
+
+    function showAllCustomers():void{
+        axios.get<ICustomer[]>(uri)
+        .then(function (response:AxiosResponse<ICustomer[]>):void{
+
+            let result : string = "<ol>";
+            response.data.forEach((customer : ICustomer) =>{
+                if (customer == null)
+                    {
+                        result
+                         += "<li>Null element</li>"
+                    }
+                else
+                    {
+                        result += "<li>" + "<br>" + "<b>Id: </b>" + customer.id.toString() + "<br>" + "<b>Fornavn: </b>" + customer.firstName + "<br>" + "<b>EfterNavn: </b>" + customer.lastName + "<br>" + "<b>Oprettet i: </b>" + customer.year.toString() + "</li>"; 
+                    }    
+            });
+
+            result += "</ol>";
+            showAllCustomerElement.innerHTML = result;
+        })
+    }
+
+
